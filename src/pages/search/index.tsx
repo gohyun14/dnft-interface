9717dad4ef9b4c3cc79f03987b18e3fbe663a533
Gabe Hyun <gabe.hyun@gmail.com>
2022-10-04 16:33:08 -0700

    fixed escaped chars search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -12,12 +12,12 @@ const SearchPage: NextPage = () => {
       </Head>
       <div className="mx-auto mt-28 w-4/5 text-center">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
-          Search Any Wallet's NFTs
+          Search Any Wallet&apos;s NFTs
         </h1>
         <p className="mb-6 text-lg font-normal text-gray-500 sm:px-16 lg:text-xl xl:px-48">
           Search up any valid wallet address, and you can see all of the NFTs
           they hold! Click on an NFT to see more specific information such as
-          the NFT's description and traits!
+          the NFT&apos;s description and traits!
         </p>
         <section></section>
       </div>
